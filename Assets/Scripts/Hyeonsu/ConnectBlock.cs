404e5f597de6a315f7d49f4e849dc147d5f4d2f6
Gangyeong Lee <gang7994@naver.com>
2023-11-21 23:50:11 +0900

    연결 스크립트 추가

---
+++ Assets/Scripts/Hyeonsu/ConnectBlock.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectBlock : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.ComparTag("Right")) {
+            Transform parentTransform = collision.transform.parent;
+            Vector3 parentPosition = parentTransform.position;
+            float parentWidth = parentTransform.localScale.x;
+
+            Vector3 newPosition = new Vector3(parentPosition.x + parentWidth, parentPosition.y, 0);
+            transform.position = newPosition;
+        }
+        else if(collision.gameObject.ComparTag("Down")) {
+            Transform parentTransform = collision.transform.parent;
+            Vector3 parentPosition = parentTransform.position;
+            float parentHeight = parentTransform.localScale.y;
+
+            Vector3 newPosition = new Vector3(parentPosition.x, parentPosition.y - parentHeight, 0);
+            transform.position = newPosition;
+        }
+    }
+}
