bc759dad3935201c08e00ad6ded73edd5f2e5052
Gangyeong Lee <gang7994@naver.com>
2023-11-23 21:24:52 +0900

    조건 추가

--- Assets/Scripts/Hyeonsu/ConnectBlock.cs
+++ Assets/Scripts/Hyeonsu/ConnectBlock.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 public class ConnectBlock : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.CompareTag("Right")) {
-            Transform parentTransform = collision.transform.parent;
-            Vector3 parentPosition = parentTransform.position;
-            float parentWidth = parentTransform.localScale.x;
+        if(!this.gameObject.GetComponent<FollowMouse>().isDown) {
+            if(collision.gameObject.CompareTag("Right")) {
+                Transform parentTransform = collision.transform.parent;
+                Vector3 parentPosition = parentTransform.position;
+                float parentWidth = parentTransform.localScale.x;
 
-            Vector3 newPosition = new Vector3(parentPosition.x + parentWidth, parentPosition.y, 0);
-            transform.position = newPosition;
-            Debug.Log("right");
-        }
-        else if(collision.gameObject.CompareTag("Down")) {
-            Transform parentTransform = collision.transform.parent;
-            Vector3 parentPosition = parentTransform.position;
-            float parentHeight = parentTransform.localScale.y;
+                Vector3 newPosition = new Vector3(parentPosition.x + parentWidth, parentPosition.y, 0);
+                transform.position = newPosition;
+                Debug.Log("right");
+            }
+            else if(collision.gameObject.CompareTag("Down")) {
+                Transform parentTransform = collision.transform.parent;
+                Vector3 parentPosition = parentTransform.position;
+                float parentHeight = parentTransform.localScale.y;
 
-            Vector3 newPosition = new Vector3(parentPosition.x, parentPosition.y - parentHeight, 0);
-            transform.position = newPosition;
-            Debug.Log("Down");
+                Vector3 newPosition = new Vector3(parentPosition.x, parentPosition.y - parentHeight, 0);
+                transform.position = newPosition;
+                Debug.Log("Down");
+            }
         }
     }
 }
