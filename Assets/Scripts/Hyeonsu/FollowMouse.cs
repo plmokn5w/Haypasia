9b61f7f0ebd3596bd743348bc8e0bcf22c6b56d8
k2901ss004 <43126013+k2901ss004@users.noreply.github.com>
2023-11-23 21:34:58 +0900

    블럭 별 드래그 앤 드롭 기능 추가
    int, string 블록 제외

--- Assets/Scripts/Hyeonsu/FollowMouse.cs
+++ Assets/Scripts/Hyeonsu/FollowMouse.cs
@@ -1,43 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class FollowMouse : MonoBehaviour
+public class FollowMouse : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    private bool isFollowing = false;
+    public static Vector2 DefaultPos;
+    public bool isDown = false;
 
-    void Update()
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
-        // 마우스 왼쪽 버튼을 누르면
-        if (Input.GetMouseButtonDown(0))
-        {
-            // 마우스 위치에 대한 Ray를 생성
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+        DefaultPos = this.transform.position;
+        isDown = true;
+    }
 
-            // Ray가 어떤 Collider와 충돌했는지 확인
-            if (hit.collider != null)
-            {
-                
-                if (hit.collider.gameObject.CompareTag("Block"))
-                {
-                    // 여기에 원하는 동작을 추가
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        Vector2 currentPos = eventData.position; 
+        this.transform.position = currentPos;
+        
+   
+    }
 
-            
-                    transform.localPosition = mousePosition;
-                }
-            }
-        }
-    
-    /*
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 mousePosition = Input.mousePosition;
-
-            
-            transform.position = mousePosition;
-        }
-
-        */
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+        this.transform.position = eventData.position;
+        Debug.Log(mousePos);
+        isDown = false;
+        
     }
 }
